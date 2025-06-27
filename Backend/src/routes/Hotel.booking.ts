1137d57d79dbd4bbd52ad4a0dbe2bde5cae223dd
Ndahayo kevin <ndahayokevin1@gmail.com>
2025-06-27 21:30:20 +0200

    Created an endpoint to verify payment status for a booking

--- Backend/src/routes/Hotel.booking.ts
+++ Backend/src/routes/Hotel.booking.ts
@@ -1,11 +1,28 @@
 // Routes (booking.routes.ts)
 import { Router, Request, Response } from "express";
 import { BookingService } from "../services/Hotel.booking";
+import { bookingRepository } from "../repository/Hotel.booking";
+import { HttpStatusCode } from "axios";
 
 export const BookingRoutes = Router();
 
 BookingRoutes.post('/create/:hotelId/:roomTypeId', (req: Request, res: Response) => {
   return BookingService.createBooking(req, res);
+});
+
+
+// Verify payment status for a booking
+BookingRoutes.get('/:booking_id/verify-payment', async (req: Request, res: Response) => {
+  try {
+    const result = await bookingRepository.verifyBookingPayment(req);
+    return res.status(result.status).json(result);
+  } catch (error) {
+    return res.status(HttpStatusCode.InternalServerError).json({
+      data: null,
+      message: 'Internal server error',
+      status: 500
+    });
+  }
 });
 
 BookingRoutes.get('/user/:userId', (req: Request, res: Response) => {
