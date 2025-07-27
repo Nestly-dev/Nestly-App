86f7aff4442eac6c154074be4112f8c46e1ea698
Ndahayo kevin <ndahayokevin1@gmail.com>
2025-07-27 16:51:39 +0200

    feature/software-glitches: added a service to handle glitch resources

--- Backend/src/services/Client.complaints.ts
+++ Backend/src/services/Client.complaints.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from 'express';
 import { HttpStatusCodes } from '../utils/helpers';
-import { sendComplainsEmail } from '../repository/sendComplainsEmail';
+import { sendComplainsEmail } from '../repository/sendEmails';
 import { complaintsDataType } from '../utils/types';
+import { emailingOptions } from '../utils/EmailingTemplates';
+import { softwareGlitchEmail } from '../repository/sendEmails';
+
+export interface softwareGlitchComplaints {
+  name?: string,
+  email?: string,
+  userRole?: string,
+  issueDescription?: string,
+  severity?: 'Low' | 'Medium' | 'High' | 'Critical'
+}
 
 export class Complaints {
   async sendComplaints(req: Request, res: Response) {
@@ -23,6 +33,30 @@ export class Complaints {
 
     } catch (error) {
       console.error("Error sending complaint email:", error);
+      return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Failed to send complaint." });
+    }
+  }
+
+  async softwareGlitchComplaints(req: Request, res: Response) {
+    try {
+      const data = {
+        email: req.user?.email as string,
+        username: req.user?.username as string,
+        role: req.user?.role as string
+      }
+
+      const severity = req.body.severity;
+      const issueDescription = req.body.issueDescription;
+
+      const glitchEmail = emailingOptions.softwareGlitchReport(data.username, data.email, data.role, issueDescription, severity);
+
+      await softwareGlitchEmail({
+        username: data.username as string,
+        emailTemplate: glitchEmail
+      });
+      return res.status(HttpStatusCodes.OK).json({ message: "Software Glitch sent successfully." });
+    } catch (error) {
+      console.error("Error sending glitch report:", error);
       return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Failed to send complaint." });
     }
   }
