d6d42631bd48799b88febdb7e4d84b5bd45b1e42
Ndahayo kevin <ndahayokevin1@gmail.com>
2025-07-27 16:48:37 +0200

    feature/permissions: granted certain degree of permissions to hotel post routes depending on user roles.

--- Backend/src/routes/Hotel.posts.ts
+++ Backend/src/routes/Hotel.posts.ts
@@ -3,6 +3,7 @@ import { Request, Response, Router } from "express";
 import { MulterRequest, upload } from "../utils/config/multer";
 import { authMiddleware } from "../middleware/authMiddleware";
 import contentAwareImageMiddleware from '../middleware/contentAwareImageMiddleware';
+import { rolesAndPermissions } from '../middleware/RolesAndPermissions';
 
 export const HotelPostRoute = Router();
 
@@ -18,20 +19,20 @@ HotelPostRoute.get('/:hotelId', (req: Request, res: Response) => {
 });
 
 // Upload Hotel Post
-HotelPostRoute.post('/upload/:hotelId', upload.single('media'), contentAwareImageMiddleware({
+HotelPostRoute.post('/upload/:hotelId', authMiddleware, rolesAndPermissions.customerNotPermitted, upload.single('media'), contentAwareImageMiddleware({
   maxWidth: 1200,
   maxHeight: 1200,
   quality: 85
-}),  authMiddleware, (req: Request, res: Response) => {
+}), (req: Request, res: Response) => {
   return hotelPostService.uploadPost(req as MulterRequest, res)
 });
 
 // Update Hotel Post details
-HotelPostRoute.patch('/update/:postId', authMiddleware, (req: Request, res: Response) => {
+HotelPostRoute.patch('/update/:postId', authMiddleware, rolesAndPermissions.customerNotPermitted, (req: Request, res: Response) => {
   return hotelPostService.updatePost(req, res)
 });
 
 // Delete media
-HotelPostRoute.delete('/delete/:postId', authMiddleware, (req: Request, res: Response) => {
+HotelPostRoute.delete('/delete/:postId', authMiddleware, rolesAndPermissions.customerNotPermitted, (req: Request, res: Response) => {
   return hotelPostService.deleteHotelPost(req, res)
 });
